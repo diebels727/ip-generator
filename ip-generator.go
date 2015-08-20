8215980530dab8468510999edb77ea90201a9fa8
Jonathan Quigg <jonathanquigg@Jonathans-MacBook-Pro.local>
2015-08-20 15:02:17 -0400

    first

---
+++ ip-generator.go
@@ -0,0 +1,54 @@
+package main
+import "fmt"
+import "math/rand"
+import "os"
+import "os/signal"
+import "time"
+
+
+func producer(ints chan<- int32,duration time.Duration) {
+  fmt.Println("launching...")
+  fmt.Println("The time to sleep is: ",duration)
+  for {
+    rand_int := rand.Int31()
+    fmt.Println("Writing to chan: ",rand_int)
+    ints <- rand_int
+    fmt.Println("Sleeping now...")
+    time.Sleep(duration)
+  }
+}
+
+func intToIP(i int32) string {
+  a := (byte(i>>24))
+  b := (byte(i>>16))
+  c := (byte(i>>8))
+  d := (byte(i))
+  str := fmt.Sprintf("%d.%d.%d.%d",a,b,c,d)
+  return str
+}
+
+func consumer(src <-chan int32,dst <- chan int32) {
+  for {
+    src_ip := <- src
+    src_str := intToIP(src_ip)
+    dst_ip := <- dst
+    dst_str := intToIP(dst_ip)
+    fmt.Println(src_str + "->" + dst_str)
+  }
+}
+
+func main() {
+  duration := 1 * time.Second
+  src := make(chan int32)
+  dst := make(chan int32)
+  notify := make(chan os.Signal,1)
+  signal.Notify(notify,os.Interrupt,os.Kill)
+
+  go producer(src,duration)
+  go producer(dst,duration)
+  go consumer(src,dst)
+
+  s := <- notify
+  fmt.Println("signal:",s)
+  fmt.Println("done.")
+}
